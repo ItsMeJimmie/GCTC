ccad8668a4435dae49146de55ab84b566f2e5728
Jimmie Cox <jamescox995@gmail.com>
2025-03-22 00:43:50 -0400

    Added paralax, new logo, and fixed a few bugs like z-index failure for overlay

---
+++ script.js
@@ -0,0 +1,10 @@
+function adjustPadding() {
+    const header = document.querySelector('.top');
+    const seperator = document.querySelector('.seperator');
+    if (seperator) {
+        seperator.style.paddingTop = header.offsetHeight + 'px';
+    }
+}
+
+window.addEventListener('load', adjustPadding);
+window.addEventListener('resize', adjustPadding);
